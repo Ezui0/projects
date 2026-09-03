17089dca1d7d359a807a761b8e19f76f7b7a233d
Rico Ardiansyah <festyus09@gmail.com>
2026-09-04 01:14:26 +0700

    Update audio.py

--- infer/lib/audio.py
+++ infer/lib/audio.py
@@ -4,9 +4,9 @@ import av
 from io import BytesIO
 
 def wav2(i, o, format):
-    inp = av.open(i, 'rb')
+    inp = av.open(i, 'r')
     if format == "m4a": format = "mp4"
-    out = av.open(o, 'wb', format=format)
+    out = av.open(o, 'w', format=format)
     if format == "ogg": format = "libvorbis"
     if format == "mp4": format = "aac"
 
@@ -21,8 +21,8 @@ def wav2(i, o, format):
     inp.close()
 
 def audio2(i, o, format, sr):
-    inp = av.open(i, 'rb')
-    out = av.open(o, 'wb', format=format)
+    inp = av.open(i, 'r')
+    out = av.open(o, 'w', format=format)
     if format == "ogg": format = "libvorbis"
     if format == "f32le": format = "pcm_f32le"
 
